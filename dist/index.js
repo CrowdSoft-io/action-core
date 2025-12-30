32969da9b609e86ced00668e684ccca3d990040a
Mihailo Chehivsky <mihailo.chehivsky@gmail.com>
2025-12-30 19:52:43 +0200

    Fixed target branch

--- dist/index.js
+++ dist/index.js
@@ -151,7 +151,7 @@ let ContextFactory = class ContextFactory {
         const remoteWwwRoot = `${remoteHomeDir}/www`;
         const remoteReleasesRoot = `${remoteHomeDir}/releases/${repository}`;
         const remoteBuildDir = `${remoteReleasesRoot}/build-${version}`;
-        console.log(githubContext);
+        console.log(JSON.stringify(githubContext, null, 2));
         return {
             repositoryName: repository,
             projectName: repository.replace(/^(\w+)-.*$/g, "$1"),
